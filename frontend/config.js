70bee1e9ad7c570f066361377dadc81e43a7d24c
site-developandmanage <aleksandraborien75@gmail.com>
2025-05-09 01:31:09 +0200

    Correction de l'intégration Stripe dans index et config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,9 @@
 // frontend/config.js
 
 // Clé publique Stripe - visible côté client
-window.PUBLIC_KEY = "pk_test_51RLeF22c6R0Sx3zFHjgPN29ClVmcE53wviw76aivAdALpMyHFqTZdpqyKbCAcoV1HV3xdRL3kqgRsylGyA8m4laX00n7I1BKq3";
+const PUBLIC_KEY = "pk_test_51RLeF22c6R0Sx3zFHjgPN29ClVmcE53wviw76aivAdALpMyHFqTZdpqyKbCAcoV1HV3xdRL3kqgRsylGyA8m4laX00n7I1BKq3";
 
 // Price ID Stripe
-window.PRICE_ID = "price_1RLeL52c6R0Sx3zFTTRSDKHB";
+const PRICE_ID = "price_1RLeL52c6R0Sx3zFTTRSDKHB";
+
+export { PUBLIC_KEY, PRICE_ID };
